03821a82dadf77a5c31a72486006ceabe0f08fba
kabziński.grzegorz <209343@edu.p.lodz.pl>
2019-03-05 18:31:53 +0100

    added whole logic for tails generator

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,10 +6,16 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        if(value == null){
+        if (value == null) {
             return new ArrayList<>(0);
         }
-        return new ArrayList<>(value.length());
+        ArrayList<String> listOfTrimmedStrings = new ArrayList<>();
+        listOfTrimmedStrings.add(value);
+        for (int i = 0; i < value.length(); i++) {
+            listOfTrimmedStrings.add(value.substring(i + 1, value.length()));
+        }
+        System.out.println(listOfTrimmedStrings);
+        return listOfTrimmedStrings;
     }
 
 }
